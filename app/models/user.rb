8b1d5b3b9de75c6e261d5c30814695c1d015668d
DanukaH <danunilminda@yahoo.com>
2022-12-12 00:01:38 +0000

    Devise MVC

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  has_many :likes
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+
+  validates :email, presence: true,
+            format: { with: EMAIL_REGEX, message: 'Invalid Email' },
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 4, maximum: 254 },
+            confirmation: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
+  }
+
+  devise_scope :user do
+    get '/login' => 'users/sessions#new'
+    get '/logout' => 'devise/sessions#destroy'
+    get '/users/confirm' => 'registrations#confirm'
+    post '/users/invitations' => 'users/invitations#resend'
+  end
 
   get 'home/index'
   root to: 'home#index'
